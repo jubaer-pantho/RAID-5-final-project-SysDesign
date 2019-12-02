39c92b6ec7d76b3e514e8cd75794d463064ecb01
jubaer-pantho <jubaer.pantho@gmail.com>
2019-12-02 00:39:08 -0500

    Add try catch method on FileSystem.py

--- FileSystem.py
+++ FileSystem.py
@@ -68,20 +68,35 @@ if __name__ == '__main__':
     #YOU MAY WRITE YOUR CODE AFTER HERE
 
     while True:
-        inputs = (raw_input("Type the desidered operation: $ ").split(' '))
+        inputs = (raw_input("Type the desired operation: $ ").split(' '))
 
         if(inputs[0] == 'mkdir'):
-            my_object.mkdir(inputs[1])
+            try:
+                my_object.mkdir(inputs[1])
+            except:
+                print("Incorrect input, Input format to create folder is <mkdir yourFolderName>")
         elif (inputs[0] == 'create'):
-           my_object.create(inputs[1])
+            try:
+                my_object.create(inputs[1])
+            except:
+                 print("Incorrect input, Input format to create file is <create yourFileName>")
         elif (inputs[0] == 'write'):
-           my_object.write(inputs[1], inputs[2], int(float(inputs[3])), int(float(inputs[4])))
+            try:
+                my_object.write(inputs[1], inputs[2], int(float(inputs[3])), int(float(inputs[4])))
+            except:
+                print("Incorrect input, Input format to write to a file is <write AbsoluteFilePath data offset delay>")
         elif (inputs[0] == 'read'):
-           my_object.read(inputs[1], int(float(inputs[2])), int(float(inputs[3])))
+            try:
+                my_object.read(inputs[1], int(float(inputs[2])), int(float(inputs[3])))
+            except:
+                print("Incorrect input, Input format to read from a file is <read AbsoluteFilePath offset readSize>")
         elif (inputs[0] == 'status'):
            my_object.status()
         elif (inputs[0] == 'rm'):
-           my_object.rm(inputs[1])
+            try:
+                my_object.rm(inputs[1])
+            except:
+                print("Incorrect input, Input format to remove a file/folder is <rm AbsoluteFilePath>")
         #elif (inputs[0] == 'mv'):
           # my_object.mv(inputs[1], inputs[2])
         else:
@@ -89,76 +104,3 @@ if __name__ == '__main__':
 
 
 
-
-        #else:
-
-
-
-
-    #Examples:
-    '''my_object.mkdir("/A")
-    #my_object.mkdir("/A/B")
-    #my_object.status()
-    $ mkdir("/A")
-    $ mkdir("/B")
-    $ mkdir("/C")
-    $ mkdir("/D")
-
-    #my_object.mkdir("/A/B")
-
-    my_object.create("/A/1.txt")
-    my_object.create("/A/2.txt")
-    string = 2048*'a'
-
-    my_object.write("/A/1.txt",string,0)
-    my_object.read("/A/1.txt",0,2047) #test1
-    #my_object.write("/A/1.txt",'bbbbb',0)
-    #my_object.status()#test2
-    #test2
-    #my_object.write("/A/1.txt",' wie gehts',0)
-    #my_object.create("/A/asasd.txt")
-    #my_object.write("/A/asasd.txt",' hooi',0) #test2
-    my_object.status()'''
-    #my_object.status()#test2'''
-
-    '''my_object.read("/A/1.txt",0,6) #test1
-    my_object.mkdir("/B")
-    my_object.write("/A/1.txt",' wie gehts',5) #test2
-    my_object.read("/A/1.txt",0,15) #test1
-    my_object.mv("/A/1.txt", "/B/")
-    my_object.rm("/B/1.txt")
-    my_object.status()'''
-    '''my_object.create("/A/2.txt")#, as A is already there we can crete file in A
-    my_object.status()
-
-
-
-
-
-    my_object.write("/A/2.txt",'01234567',0) #test3
-    #my_object.read("/A/1.txt",2,2) #test3/4
-
-    #my_object.write("/A/2.txt",'reset\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"',0) #test5/6
-    #my_object.read("/A/2.txt",0,5) #test5/6
-
-
-    interface.link("/A/2.txt","/A/B/3.txt")
-    my_object.rm("/A/2.txt")
-    my_object.read("/A/B/3.txt",0,4) #test3/4
-
-    my_object.status()
-    #my_object.write("/A/1.txt",'hallo',0)
-    #my_object.read("/B/3.txt",1,4)
-
-    #my_object.rm("/A")
-    #my_object.rm("/A/1.txt")'''
-    '''my_object.mv("/A/B/1.txt", "/")
-
-    my_object.rm("/A")
-    my_object.status()'''
-    '''my_object.status()
-
-
-
-    my_object.status()
-    '''
